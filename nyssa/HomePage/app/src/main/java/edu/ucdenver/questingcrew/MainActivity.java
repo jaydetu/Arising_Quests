9e9574947ec2a3410cd7031e8120624ae016b1b3
nyssa-jordan <jordannyssa@gmail.com>
2023-11-27 22:20:33 -0700

    Updated package name
    Changed package name from com.example.homepage to edu.ucdenver.questingcrew.arisingquests.

--- nyssa/HomePage/app/src/main/java/edu/ucdenver/questingcrew/MainActivity.java
+++ nyssa/HomePage/app/src/main/java/edu/ucdenver/questingcrew/MainActivity.java
@@ -1,11 +1,11 @@
-package com.example.homepage;
+package edu.ucdenver.questingcrew;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
+import edu.ucdenver.questingcrew.arisingquests.R;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -15,7 +15,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.homepage.databinding.ActivityMainBinding;
+import edu.ucdenver.questingcrew.arisingquests.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
--- nyssa/HomePage/app/src/main/java/edu/ucdenver/questingcrew/arisingquests/ui/gallery/GalleryFragment.java
+++ nyssa/HomePage/app/src/main/java/edu/ucdenver/questingcrew/arisingquests/ui/gallery/GalleryFragment.java
@@ -1,4 +1,4 @@
-package com.example.homepage.ui.gallery;
+package edu.ucdenver.questingcrew.arisingquests.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.homepage.databinding.FragmentGalleryBinding;
+import edu.ucdenver.questingcrew.arisingquests.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
