910064de6a9fd372a45bc28118ade3aa934c9723
GucciRemyBoi <renz.wilkins@live.com>
2023-11-29 20:13:16 -0700

    added back to home button to mental resources page

--- app/src/main/java/edu/ucdenver/questingcrew/arisingquests/MentalHealth.java
+++ app/src/main/java/edu/ucdenver/questingcrew/arisingquests/MentalHealth.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +19,11 @@ public class MentalHealth extends AppCompatActivity {
 
         Button homeButton = findViewById(R.id.homeButton);
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(MentalHealth.this, MainActivity.class);
+        homeButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MentalHealth.this, MainActivity.class);
 
-                startActivity(intent);
-                finish();
-            }
+            startActivity(intent);
+            finish();
         });
 
         Button takelinkbtn = findViewById(R.id.take_this_btn);
